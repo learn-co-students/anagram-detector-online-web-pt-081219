eaaf0b2bb0b4ba25d6fa900430c357df62a4df12
imperative-genre-4923 <dreama.lin@yale.edu>
2019-09-14 23:14:00 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(some_word)
+    @@match = []
+    some_word.each { |e|
+      if e.split("").sort == @word.split("").sort
+        @@match << e
+      end
+      }
+      @@match
+  end
+end
