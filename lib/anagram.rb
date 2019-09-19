1e80d2a394bf3b8b659728e248cf1b694af007d1
wifelette <leahsilber@gmail.com>
2019-09-18 20:47:29 -0700

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,16 +1,14 @@
-require 'pry-byebug'
-
 class Anagram
-
   attr_accessor :word
 
   def initialize(word)
     @word = word
+    @word_letters = @word.chars.sort
   end
 
   def match(array_of_words)
     array_of_words.select do |dictionary_word|
-      @word.chars.sort == dictionary_word.chars.sort
+      @word_letters == dictionary_word.chars.sort
     end
   end
 end
