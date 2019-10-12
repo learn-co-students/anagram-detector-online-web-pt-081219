1b0db8f9cec3022e03e204e7da105b44e1a6b21f
arnie b <arnold.bwaila@gmail.com>
2019-10-12 13:38:54 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :name
+  @name =''
+  def initialize(word)
+    name = word
+  end
+  
+  def match(anagrams)
+    %w(anagrams).collect {|anagram| anagram.sort == @name.sort}
+  end
+end #class end
